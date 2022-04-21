f992c4942d1878a4172f5a40cfa6ce6c65d6543b
Zanchi <zanchi@hey.com>
2022-04-21 02:31:39 -0400

    create `useSelections` to track selection state

---
+++ src/hooks/useSelections.ts
@@ -0,0 +1,62 @@
+import { useState } from 'preact/hooks'
+
+/**
+ * a cell location, [rowIndex, columnIndex].
+ */
+export type Selection = [number, number]
+
+type Actions = {
+  /**
+   * add a new selection while keeping existing ones.
+   */
+  add: (selection: Selection) => void
+  /**
+   * clear selections and return the last state.
+   */
+  clear: () => Selection[]
+  /**
+   * get the selected columns for the given row.
+   */
+  getColumns: ReturnType<typeof getRowSelections>
+  /**
+   * replace current selections.
+   */
+  set: (selections: Selection[]) => void
+}
+
+const getRowSelections =
+  (selections: Selection[]) =>
+  (row: number): Set<number> => {
+    const columns = selections
+      .filter(([r]) => r === row)
+      .map(([, column]) => column)
+
+    return new Set(columns)
+  }
+
+/**
+ * returns the current selection state as an array.
+ * selections are in order, from first to last.
+ */
+const useSelections = (
+  initialSelections: Selection[] = []
+): [Selection[], Actions] => {
+  const [selections, setSelections] = useState<Selection[]>(initialSelections)
+
+  const add = ([row, column]: Selection) =>
+    setSelections((s) => [...s, [row, column]])
+
+  const clear = () => {
+    setSelections([])
+    return selections
+  }
+
+  const set = (selections: Selection[]) => setSelections(selections)
+
+  return [
+    selections,
+    { add, clear, getColumns: getRowSelections(selections), set },
+  ]
+}
+
+export default useSelections
