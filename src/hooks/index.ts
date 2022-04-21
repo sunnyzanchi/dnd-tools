4c4a568e3ea7f333504a7c45c74c9e6ebbb8bea9
Zanchi <zanchi@hey.com>
2022-04-21 02:24:49 -0400

    create `useKeyIsPressed` hook
    convenience wrapper around `useKeyBind` and `useKeyUp`
    to get a boolean value

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { default as useClickOutside } from './useClickOutside'
 export { default as useElementSize } from './useElementSize'
+export { default as useKeyIsPressed } from './useKeyIsPressed'
 export { default as useScreenSize } from './useScreenSize'
 export { default as useStack } from './useStack'
