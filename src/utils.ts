489fc1bb0121a9b379e312e117998740004faf37
Zanchi <zanchi@hey.com>
2022-05-07 19:40:50 -0400

    add doc comment and improve types

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,25 @@
+/**
+ * return a new object with the given `prop` updated to
+ * `fn`, or the return value of `fn` if it's a function.
+ *
+ * @example
+ *
+ * ```js
+ * const o1 = { counter: 1, name: 'Hasselhoff' }
+ * const incrementCounter = mapProp('counter', x => x + 1)
+ * const o2 = incrementCounter(o1)
+ * // { counter: 2, name: 'Hasselhoff' }
+ * ```
+ */
 export const mapProp =
   <T extends Record<string | number | symbol, T[keyof T]>, U extends keyof T>(
     prop: U,
-    fn: (value: T[U]) => T[U]
+    fn: T[U] | ((value: T[U]) => T[U])
   ) =>
   (obj: T): T => ({
     ...obj,
-    [prop]: fn(obj[prop]),
+    // @ts-expect-error
+    [prop]: fn?.(obj[prop]) ?? fn,
   })
 
 export const append =
