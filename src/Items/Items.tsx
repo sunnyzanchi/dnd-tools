2a58aa60821792ef3cbc58929b4433198b9e0541
Zanchi <zanchi@hey.com>
2022-05-08 22:34:44 -0400

    add search to Items

--- src/Items/Items.tsx
+++ src/Items/Items.tsx
@@ -1,29 +1,41 @@
+import compose from 'compose-function'
 import { useState } from 'preact/hooks'
 
+import { getInputVal } from 'src/Creatures/utils'
 import { SearchHeader } from 'src/components'
 import { ListItem } from 'src/components'
 import ExpandedItem from './ExpandedItem'
+import { Item } from './types'
 import useItems from './useItems'
 import styles from './Items.module.scss'
+import { matchSorter } from 'match-sorter'
 
 const Items = () => {
   const [items] = useItems()
   const [expanded, setExpanded] = useState<number | null>(null)
   const [selected, setSelected] = useState<number | null>(null)
+  const [searchTerm, setSearchTerm] = useState('')
 
   const deselect = () => setExpanded(null)
   const select = (index: number) => () => setExpanded(index)
+
+  let filteredItems: Item[]
+  if (searchTerm !== '') {
+    filteredItems = matchSorter(items, searchTerm, { keys: ['name'] })
+  } else {
+    filteredItems = items
+  }
 
   return (
     <section class={styles.container}>
       <SearchHeader
         onAdd={() => {}}
-        onInput={() => {}}
-        searchTerm={''}
+        onInput={compose(setSearchTerm, getInputVal)}
+        searchTerm={searchTerm}
         title="Magic Items"
       />
       <ol class={styles.items}>
-        {items.map((item, i) =>
+        {filteredItems.map((item, i) =>
           expanded === i ? (
             <ExpandedItem onCollapse={deselect} {...item} />
           ) : (
